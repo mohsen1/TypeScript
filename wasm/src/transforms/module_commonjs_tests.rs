19531290f17b5a88e2a03d45b4cd975b53088d5b
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-08 15:47:41 +0100

    [wasm] transforms: cover hyphenated string exports

--- wasm/src/transforms/module_commonjs_tests.rs
+++ wasm/src/transforms/module_commonjs_tests.rs
@@ -475,6 +475,27 @@ fn test_collect_export_names_with_multiple_named_exports() {
 }
 
 #[test]
+fn test_collect_export_names_with_string_named_exports() {
+    use crate::thin_parser::ThinParserState;
+
+    let source = "const foo = 1; export { foo as \"foo-bar\" };";
+    let mut parser = ThinParserState::new("test.ts".to_string(), source.to_string());
+    let root = parser.parse_source_file();
+
+    let Some(source_file) = parser.arena.get_source_file(parser.arena.get(root).unwrap()) else {
+        panic!("Failed to get source file");
+    };
+
+    let export_names = collect_export_names(&parser.arena, &source_file.statements.nodes);
+
+    assert_eq!(
+        export_names,
+        vec!["foo-bar"],
+        "Expected string-named export to be collected"
+    );
+}
+
+#[test]
 fn test_collect_export_names_with_export_import_equals() {
     use crate::thin_parser::ThinParserState;
 
