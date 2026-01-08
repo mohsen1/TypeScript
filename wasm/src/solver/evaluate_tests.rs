3c051b25829ea53f9aa230d0e5b54166875f35b2
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-08 16:11:45 +0100

    [wasm] solver: add never object infer test

--- wasm/src/solver/evaluate_tests.rs
+++ wasm/src/solver/evaluate_tests.rs
@@ -8354,6 +8354,38 @@ fn test_conditional_infer_template_literal_never_input() {
 }
 
 #[test]
+fn test_conditional_infer_object_never_input() {
+    let interner = TypeInterner::new();
+
+    let r_name = interner.intern_string("R");
+    let infer_r = interner.intern(TypeKey::Infer(TypeParamInfo {
+        name: r_name,
+        constraint: None,
+        default: None,
+    }));
+
+    // never extends { a: infer R } ? R : string -> never
+    let extends_obj = interner.object(vec![PropertyInfo {
+        name: interner.intern_string("a"),
+        type_id: infer_r,
+        write_type: infer_r,
+        optional: false,
+        readonly: false,
+        is_method: false,
+    }]);
+    let cond = ConditionalType {
+        check_type: TypeId::NEVER,
+        extends_type: extends_obj,
+        true_type: infer_r,
+        false_type: TypeId::STRING,
+        is_distributive: false,
+    };
+
+    let result = evaluate_conditional(&interner, &cond);
+    assert_eq!(result, TypeId::NEVER);
+}
+
+#[test]
 fn test_conditional_infer_constraint_mismatch() {
     let interner = TypeInterner::new();
 
