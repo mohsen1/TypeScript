3cfb86b4684630c3a93c5759704b67a2b863afad
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 22:38:12 -0600

    Worker worker-5: Enum and Namespace Support

--- target/debug/deps/ts_parser-629ac2772afcc2d4.d
+++ target/debug/deps/ts_parser-629ac2772afcc2d4.d
@@ -1,10 +1,15 @@
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs
 
 src/lib.rs:
+src/binder/mod.rs:
+src/binder/enums.rs:
+src/checker/mod.rs:
+src/checker/enums.rs:
+src/checker/namespaces.rs:
 src/parser/mod.rs:
 src/parser/error_recovery.rs:
 src/parser/statements.rs:
@@ -12,3 +17,5 @@ src/parser/expressions.rs:
 src/parser/types.rs:
 src/parser/parser.rs:
 src/thin_parser.rs:
+src/transforms/mod.rs:
+src/transforms/enums.rs:
--- target/debug/deps/ts_parser-99d7c3929d5e57c0.d
+++ target/debug/deps/ts_parser-99d7c3929d5e57c0.d
@@ -1,8 +1,13 @@
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-99d7c3929d5e57c0.d: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-99d7c3929d5e57c0.d: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-99d7c3929d5e57c0: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-99d7c3929d5e57c0: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs
 
 src/lib.rs:
+src/binder/mod.rs:
+src/binder/enums.rs:
+src/checker/mod.rs:
+src/checker/enums.rs:
+src/checker/namespaces.rs:
 src/parser/mod.rs:
 src/parser/error_recovery.rs:
 src/parser/statements.rs:
@@ -10,3 +15,5 @@ src/parser/expressions.rs:
 src/parser/types.rs:
 src/parser/parser.rs:
 src/thin_parser.rs:
+src/transforms/mod.rs:
+src/transforms/enums.rs:
