55faddf4e8fbe8cf8a1c6e3831de8b2b46f28722
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 22:29:56 -0600

    Worker worker-5: Parser Core

--- target/debug/deps/ts_parser-629ac2772afcc2d4.d
+++ target/debug/deps/ts_parser-629ac2772afcc2d4.d
@@ -1,10 +1,14 @@
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/thin_parser.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/thin_parser.rs
 
 src/lib.rs:
 src/parser/mod.rs:
 src/parser/error_recovery.rs:
+src/parser/statements.rs:
+src/parser/expressions.rs:
+src/parser/types.rs:
+src/parser/parser.rs:
 src/thin_parser.rs:
