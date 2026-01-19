11395efdb505d9f5610816d0b93abce1fd1ab949
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 22:52:59 -0600

    Worker worker-5: Union Type Discrimination and Exhaustiveness

--- target/debug/deps/ts_parser-629ac2772afcc2d4.d
+++ target/debug/deps/ts_parser-629ac2772afcc2d4.d
@@ -1,8 +1,8 @@
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/ts_parser-629ac2772afcc2d4.d: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/exhaustiveness.rs src/checker/namespaces.rs src/checker/union.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/solver/union.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rlib: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/exhaustiveness.rs src/checker/namespaces.rs src/checker/union.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/solver/union.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/namespaces.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-5/target/debug/deps/libts_parser-629ac2772afcc2d4.rmeta: src/lib.rs src/binder/mod.rs src/binder/enums.rs src/checker/mod.rs src/checker/compatibility.rs src/checker/enums.rs src/checker/exhaustiveness.rs src/checker/namespaces.rs src/checker/union.rs src/parser/mod.rs src/parser/error_recovery.rs src/parser/statements.rs src/parser/expressions.rs src/parser/types.rs src/parser/parser.rs src/solver/mod.rs src/solver/intersection.rs src/solver/union.rs src/thin_parser.rs src/transforms/mod.rs src/transforms/enums.rs src/types/mod.rs src/types/intersection.rs
 
 src/lib.rs:
 src/binder/mod.rs:
@@ -10,7 +10,9 @@ src/binder/enums.rs:
 src/checker/mod.rs:
 src/checker/compatibility.rs:
 src/checker/enums.rs:
+src/checker/exhaustiveness.rs:
 src/checker/namespaces.rs:
+src/checker/union.rs:
 src/parser/mod.rs:
 src/parser/error_recovery.rs:
 src/parser/statements.rs:
@@ -19,6 +21,7 @@ src/parser/types.rs:
 src/parser/parser.rs:
 src/solver/mod.rs:
 src/solver/intersection.rs:
+src/solver/union.rs:
 src/thin_parser.rs:
 src/transforms/mod.rs:
 src/transforms/enums.rs:
