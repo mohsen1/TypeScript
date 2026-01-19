8739292a11a147a947f550f41380d7e48100669f
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 22:42:49 -0600

    Worker worker-4: Function and Call Expression Checking

--- target/debug/deps/ts_parser-ac043ba7f5dbb6b1.d
+++ target/debug/deps/ts_parser-ac043ba7f5dbb6b1.d
@@ -1,6 +1,6 @@
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-ac043ba7f5dbb6b1.d: src/parser/lib.rs src/parser/node_kind.rs src/parser/thin_node.rs src/parser/ast/mod.rs src/parser/ast/arena.rs src/parser/ast/nodes.rs src/parser/ast/statements.rs src/parser/ast/expressions.rs src/parser/ast/types.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-ac043ba7f5dbb6b1.d: src/parser/lib.rs src/parser/node_kind.rs src/parser/thin_node.rs src/parser/ast/mod.rs src/parser/ast/arena.rs src/parser/ast/nodes.rs src/parser/ast/statements.rs src/parser/ast/expressions.rs src/parser/ast/types.rs src/parser/checker/mod.rs src/parser/checker/signatures.rs src/parser/checker/functions.rs src/parser/checker/calls.rs src/parser/checker/overloads.rs
 
-/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-ac043ba7f5dbb6b1: src/parser/lib.rs src/parser/node_kind.rs src/parser/thin_node.rs src/parser/ast/mod.rs src/parser/ast/arena.rs src/parser/ast/nodes.rs src/parser/ast/statements.rs src/parser/ast/expressions.rs src/parser/ast/types.rs
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-ac043ba7f5dbb6b1: src/parser/lib.rs src/parser/node_kind.rs src/parser/thin_node.rs src/parser/ast/mod.rs src/parser/ast/arena.rs src/parser/ast/nodes.rs src/parser/ast/statements.rs src/parser/ast/expressions.rs src/parser/ast/types.rs src/parser/checker/mod.rs src/parser/checker/signatures.rs src/parser/checker/functions.rs src/parser/checker/calls.rs src/parser/checker/overloads.rs
 
 src/parser/lib.rs:
 src/parser/node_kind.rs:
@@ -11,5 +11,10 @@ src/parser/ast/nodes.rs:
 src/parser/ast/statements.rs:
 src/parser/ast/expressions.rs:
 src/parser/ast/types.rs:
+src/parser/checker/mod.rs:
+src/parser/checker/signatures.rs:
+src/parser/checker/functions.rs:
+src/parser/checker/calls.rs:
+src/parser/checker/overloads.rs:
 
 # env-dep:CARGO_PKG_VERSION=0.1.0
