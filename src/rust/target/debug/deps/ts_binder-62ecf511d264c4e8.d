1cc0ca7f98a4eac6b00dd20191e0b46d0f1e7da5
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 20:29:46 -0600

    Worker worker-6: Binder Implementation

---
+++ src/rust/target/debug/deps/ts_binder-62ecf511d264c4e8.d
@@ -0,0 +1,12 @@
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-6/src/rust/target/debug/deps/ts_binder-62ecf511d264c4e8.d: lib.rs symbols.rs scope.rs binder.rs arena.rs syntax_kind.rs
+
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-6/src/rust/target/debug/deps/libts_binder-62ecf511d264c4e8.rlib: lib.rs symbols.rs scope.rs binder.rs arena.rs syntax_kind.rs
+
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-6/src/rust/target/debug/deps/libts_binder-62ecf511d264c4e8.rmeta: lib.rs symbols.rs scope.rs binder.rs arena.rs syntax_kind.rs
+
+lib.rs:
+symbols.rs:
+scope.rs:
+binder.rs:
+arena.rs:
+syntax_kind.rs:
---
+++ src/rust/target/debug/deps/ts_binder-dcfe19bf252470f2.d
@@ -0,0 +1,10 @@
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-6/src/rust/target/debug/deps/ts_binder-dcfe19bf252470f2.d: lib.rs symbols.rs scope.rs binder.rs arena.rs syntax_kind.rs
+
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-6/src/rust/target/debug/deps/libts_binder-dcfe19bf252470f2.rmeta: lib.rs symbols.rs scope.rs binder.rs arena.rs syntax_kind.rs
+
+lib.rs:
+symbols.rs:
+scope.rs:
+binder.rs:
+arena.rs:
+syntax_kind.rs:
