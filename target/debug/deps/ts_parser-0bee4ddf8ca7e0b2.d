b375c16fd845d2ab0639e15cd2dfa401b7773f96
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-18 22:31:19 -0600

    Worker worker-4: AST Node Definitions

---
+++ target/debug/deps/ts_parser-0bee4ddf8ca7e0b2.d
@@ -0,0 +1,5 @@
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-0bee4ddf8ca7e0b2.d: src/parser/main.rs
+
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-0bee4ddf8ca7e0b2: src/parser/main.rs
+
+src/parser/main.rs:
---
+++ target/debug/deps/ts_parser-6989627a95db6d96.d
@@ -0,0 +1,5 @@
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-6989627a95db6d96.d: src/parser/main.rs
+
+/private/var/folders/57/xp3brw212ygckhkk_ml783fr0000gn/T/cco-workspace-TypeScript-1768787246814/worktrees/worker-4/target/debug/deps/ts_parser-6989627a95db6d96: src/parser/main.rs
+
+src/parser/main.rs:
